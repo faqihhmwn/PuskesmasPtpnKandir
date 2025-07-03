bdc5bc7a0fc4ffa92f7d7305c26283d072d1e72c
airtaprtw <airtapertiwi870@gmail.com>
2025-07-03 09:17:34 +0700

    tambah laman rekap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -182,6 +182,10 @@
     <div class="nav-item">
       <a href="/content/contact">Farmasi</a>
     </div>
+
+    <div class="nav-item">
+      <a href="/content/contact">Rekapitulasi Biaya</a>
+    </div>
   </div>
 
   @auth
