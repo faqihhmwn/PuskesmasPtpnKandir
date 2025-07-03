e2306d11ac5a6500f016717deadf631a754c85e7
airtaprtw <airtapertiwi870@gmail.com>
2025-07-03 14:31:10 +0700

    perbarui rekap biaya

--- resources/views/partials/rekap-biaya.blade.php
+++ resources/views/partials/rekap-biaya.blade.php
@@ -70,11 +70,6 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Daftar bulan -->
-                    <!-- Loop manual: Jan - Dec -->
-                    <!-- Kolom pertama (bulan) dikunci -->
-                    <!-- Sisanya input manual -->
-                    <!-- JANUARI -->
                     <tr>
                         <td><input type="text" value="Jan" name="data[Jan][bulan]" readonly></td>
                         <td><input type="number" name="data[Jan][gol_3_4]"></td>
@@ -109,9 +104,158 @@
                         <td><input type="number" name="data[Feb][total]"></td>
                     </tr>
 
-                    <!-- Lanjutkan sampai Dec -->
-                    <!-- Ganti value dan name bulan: Mar, Apr, ..., Dec -->
-                    <!-- Jika ingin saya lanjutkan semua 12 bulan, tinggal beri tahu -->
+                    <tr>
+                        <td><input type="text" value="Mar" name="data[Mar][bulan]" readonly></td>
+                        <td><input type="number" name="data[Mar][gol_3_4]"></td>
+                        <td><input type="number" name="data[Mar][gol_1_2]"></td>
+                        <td><input type="number" name="data[Mar][kampanye]"></td>
+                        <td><input type="number" name="data[Mar][honor]"></td>
+                        <td><input type="number" name="data[Mar][pens_3_4]"></td>
+                        <td><input type="number" name="data[Mar][pens_1_2]"></td>
+                        <td><input type="number" name="data[Mar][direksi]"></td>
+                        <td><input type="number" name="data[Mar][dekom]"></td>
+                        <td><input type="number" name="data[Mar][pengacara]"></td>
+                        <td><input type="number" name="data[Mar][transport]"></td>
+                        <td><input type="number" name="data[Mar][hiperkes]"></td>
+                        <td><input type="number" name="data[Mar][total]"></td>
+                    </tr>
+
+                    <tr>
+                        <td><input type="text" value="Apr" name="data[Apr][bulan]" readonly></td>
+                        <td><input type="number" name="data[Apr][gol_3_4]"></td>
+                        <td><input type="number" name="data[Apr][gol_1_2]"></td>
+                        <td><input type="number" name="data[Apr][kampanye]"></td>
+                        <td><input type="number" name="data[Apr][honor]"></td>
+                        <td><input type="number" name="data[Apr][pens_3_4]"></td>
+                        <td><input type="number" name="data[Apr][pens_1_2]"></td>
+                        <td><input type="number" name="data[Apr][direksi]"></td>
+                        <td><input type="number" name="data[Apr][dekom]"></td>
+                        <td><input type="number" name="data[Apr][pengacara]"></td>
+                        <td><input type="number" name="data[Apr][transport]"></td>
+                        <td><input type="number" name="data[Apr][hiperkes]"></td>
+                        <td><input type="number" name="data[Apr][total]"></td>
+                    </tr>
+
+                    <tr>
+                        <td><input type="text" value="May" name="data[May][bulan]" readonly></td>
+                        <td><input type="number" name="data[May][gol_3_4]"></td>
+                        <td><input type="number" name="data[May][gol_1_2]"></td>
+                        <td><input type="number" name="data[May][kampanye]"></td>
+                        <td><input type="number" name="data[May][honor]"></td>
+                        <td><input type="number" name="data[May][pens_3_4]"></td>
+                        <td><input type="number" name="data[May][pens_1_2]"></td>
+                        <td><input type="number" name="data[May][direksi]"></td>
+                        <td><input type="number" name="data[May][dekom]"></td>
+                        <td><input type="number" name="data[May][pengacara]"></td>
+                        <td><input type="number" name="data[May][transport]"></td>
+                        <td><input type="number" name="data[May][hiperkes]"></td>
+                        <td><input type="number" name="data[May][total]"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" value="Jun" name="data[Jun][bulan]" readonly></td>
+                        <td><input type="number" name="data[Jun][gol_3_4]"></td>
+                        <td><input type="number" name="data[Jun][gol_1_2]"></td>
+                        <td><input type="number" name="data[Jun][kampanye]"></td>
+                        <td><input type="number" name="data[Jun][honor]"></td>
+                        <td><input type="number" name="data[Jun][pens_3_4]"></td>
+                        <td><input type="number" name="data[Jun][pens_1_2]"></td>
+                        <td><input type="number" name="data[Jun][direksi]"></td>
+                        <td><input type="number" name="data[Jun][dekom]"></td>
+                        <td><input type="number" name="data[Jun][pengacara]"></td>
+                        <td><input type="number" name="data[Jun][transport]"></td>
+                        <td><input type="number" name="data[Jun][hiperkes]"></td>
+                        <td><input type="number" name="data[Jun][total]"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" value="Jul" name="data[Jul][bulan]" readonly></td>
+                        <td><input type="number" name="data[Jul][gol_3_4]"></td>
+                        <td><input type="number" name="data[Jul][gol_1_2]"></td>
+                        <td><input type="number" name="data[Jul][kampanye]"></td>
+                        <td><input type="number" name="data[Jul][honor]"></td>
+                        <td><input type="number" name="data[Jul][pens_3_4]"></td>
+                        <td><input type="number" name="data[Jul][pens_1_2]"></td>
+                        <td><input type="number" name="data[Jul][direksi]"></td>
+                        <td><input type="number" name="data[Jul][dekom]"></td>
+                        <td><input type="number" name="data[Jul][pengacara]"></td>
+                        <td><input type="number" name="data[Jul][transport]"></td>
+                        <td><input type="number" name="data[Jul][hiperkes]"></td>
+                        <td><input type="number" name="data[Jul][total]"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" value="Aug" name="data[Aug][bulan]" readonly></td>
+                        <td><input type="number" name="data[Aug][gol_3_4]"></td>
+                        <td><input type="number" name="data[Aug][gol_1_2]"></td>
+                        <td><input type="number" name="data[Aug][kampanye]"></td>
+                        <td><input type="number" name="data[Aug][honor]"></td>
+                        <td><input type="number" name="data[Aug][pens_3_4]"></td>
+                        <td><input type="number" name="data[Aug][pens_1_2]"></td>
+                        <td><input type="number" name="data[Aug][direksi]"></td>
+                        <td><input type="number" name="data[Aug][dekom]"></td>
+                        <td><input type="number" name="data[Aug][pengacara]"></td>
+                        <td><input type="number" name="data[Aug][transport]"></td>
+                        <td><input type="number" name="data[Aug][hiperkes]"></td>
+                        <td><input type="number" name="data[Aug][total]"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" value="Sep" name="data[Sep][bulan]" readonly></td>
+                        <td><input type="number" name="data[Sep][gol_3_4]"></td>
+                        <td><input type="number" name="data[Sep][gol_1_2]"></td>
+                        <td><input type="number" name="data[Sep][kampanye]"></td>
+                        <td><input type="number" name="data[Sep][honor]"></td>
+                        <td><input type="number" name="data[Sep][pens_3_4]"></td>
+                        <td><input type="number" name="data[Sep][pens_1_2]"></td>
+                        <td><input type="number" name="data[Sep][direksi]"></td>
+                        <td><input type="number" name="data[Sep][dekom]"></td>
+                        <td><input type="number" name="data[Sep][pengacara]"></td>
+                        <td><input type="number" name="data[Sep][transport]"></td>
+                        <td><input type="number" name="data[Sep][hiperkes]"></td>
+                        <td><input type="number" name="data[Sep][total]"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" value="Oct" name="data[Oct][bulan]" readonly></td>
+                        <td><input type="number" name="data[Oct][gol_3_4]"></td>
+                        <td><input type="number" name="data[Oct][gol_1_2]"></td>
+                        <td><input type="number" name="data[Oct][kampanye]"></td>
+                        <td><input type="number" name="data[Oct][honor]"></td>
+                        <td><input type="number" name="data[Oct][pens_3_4]"></td>
+                        <td><input type="number" name="data[Oct][pens_1_2]"></td>
+                        <td><input type="number" name="data[Oct][direksi]"></td>
+                        <td><input type="number" name="data[Oct][dekom]"></td>
+                        <td><input type="number" name="data[Oct][pengacara]"></td>
+                        <td><input type="number" name="data[Oct][transport]"></td>
+                        <td><input type="number" name="data[Oct][hiperkes]"></td>
+                        <td><input type="number" name="data[Oct][total]"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" value="Nov" name="data[Nov][bulan]" readonly></td>
+                        <td><input type="number" name="data[Nov][gol_3_4]"></td>
+                        <td><input type="number" name="data[Nov][gol_1_2]"></td>
+                        <td><input type="number" name="data[Nov][kampanye]"></td>
+                        <td><input type="number" name="data[Nov][honor]"></td>
+                        <td><input type="number" name="data[Nov][pens_3_4]"></td>
+                        <td><input type="number" name="data[Nov][pens_1_2]"></td>
+                        <td><input type="number" name="data[Nov][direksi]"></td>
+                        <td><input type="number" name="data[Nov][dekom]"></td>
+                        <td><input type="number" name="data[Nov][pengacara]"></td>
+                        <td><input type="number" name="data[Nov][transport]"></td>
+                        <td><input type="number" name="data[Nov][hiperkes]"></td>
+                        <td><input type="number" name="data[Nov][total]"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" value="Dec" name="data[Dec][bulan]" readonly></td>
+                        <td><input type="number" name="data[Dec][gol_3_4]"></td>
+                        <td><input type="number" name="data[Dec][gol_1_2]"></td>
+                        <td><input type="number" name="data[Dec][kampanye]"></td>
+                        <td><input type="number" name="data[Dec][honor]"></td>
+                        <td><input type="number" name="data[Dec][pens_3_4]"></td>
+                        <td><input type="number" name="data[Dec][pens_1_2]"></td>
+                        <td><input type="number" name="data[Dec][direksi]"></td>
+                        <td><input type="number" name="data[Dec][dekom]"></td>
+                        <td><input type="number" name="data[Dec][pengacara]"></td>
+                        <td><input type="number" name="data[Dec][transport]"></td>
+                        <td><input type="number" name="data[Dec][hiperkes]"></td>
+                        <td><input type="number" name="data[Dec][total]"></td>
+                    </tr>
                 </tbody>
             </table>
 
