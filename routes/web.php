859e14d30b1a4dbb147b94dd41c48d3176705f9c
airtaprtw <airtapertiwi870@gmail.com>
2025-07-03 10:09:32 +0700

    ubah

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,4 @@ Route::get('/content/contact', function () {
 
 Route::post('/pengguna/tambah', [UserController::class, 'store'])->name('pengguna.store');
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/content/rekap-biaya', function () {
-    return view('content.rekap-biaya');
-});
 
