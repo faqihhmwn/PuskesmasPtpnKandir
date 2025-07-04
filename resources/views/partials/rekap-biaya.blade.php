64bab0367cba63c805204ff7801c3f4d2fcc48bd
airtaprtw <airtapertiwi870@gmail.com>
2025-07-04 10:00:11 +0700

    nambah button scroll tabel

--- resources/views/partials/rekap-biaya.blade.php
+++ resources/views/partials/rekap-biaya.blade.php
@@ -4,11 +4,18 @@
   <meta charset="UTF-8">
   <title>Rekapitulasi Biaya Kesehatan</title>
   <style>
+    .table-wrapper {
+      overflow-x: auto;
+      position: relative;
+      margin-top: 20px;
+      border: 1px solid #ccc;
+      padding: 10px;
+    }
+
     table {
-      width: 100%;
+      width: max-content;
       border-collapse: collapse;
-      margin-top: 30px;
-      table-layout: fixed;
+      margin: 0 auto;
     }
 
     th, td {
@@ -16,6 +23,7 @@
       padding: 8px;
       text-align: center;
       vertical-align: middle;
+      white-space: nowrap;
     }
 
     th {
@@ -32,18 +40,13 @@
       background-color: #004c78;
     }
 
-    th:first-child,
-    td:first-child {
-      width: 8%;
-    }
-
     input[type="number"],
     input[type="text"] {
-      width: 100%;
       padding: 6px;
       box-sizing: border-box;
       border: 1px solid #ccc;
       border-radius: 4px;
+      min-width: 100px;
     }
 
     input[readonly] {
@@ -64,67 +67,97 @@
     .submit-btn:hover {
       background-color: #005f99;
     }
+
+    .scroll-buttons {
+      display: flex;
+      justify-content: center;
+      margin: 10px 0;
+    }
+
+    .scroll-buttons button {
+      margin: 0 5px;
+      padding: 8px 16px;
+      border: none;
+      border-radius: 5px;
+      background-color: #0077c0;
+      color: white;
+      cursor: pointer;
+      font-weight: bold;
+    }
+
+    .scroll-buttons button:hover {
+      background-color: #005f99;
+    }
   </style>
 </head>
 <body>
   <div class="container">
     <h2>Rekapitulasi Biaya Kesehatan</h2>
-    <form method="POST" action="/rekap-biaya/simpan">
-      <table>
-        <thead>
-          <tr>
-            <th rowspan="2">Rekap Bulan</th>
-            <th colspan="9" class="group-header">REAL BIAYA</th>
-            <th rowspan="2">Transport</th>
-            <th rowspan="2">Hiperkes</th>
-            <th rowspan="2">Total Biaya Kesehatan</th>
-          </tr>
-          <tr>
-            <th>Gol. III-IV</th>
-            <th>Gol. I-II</th>
-            <th>Kampanye</th>
-            <th>Honor + ILA + OS</th>
-            <th>Pens. III-IV</th>
-            <th>Pens. I-II</th>
-            <th>Direksi</th>
-            <th>Dekom</th>
-            <th>Pengacara</th>
-          </tr>
-        </thead>
-        <tbody>
-                                <tr>
-                        <td><input type="text" value="Jan" name="data[Jan][bulan]" readonly></td>
-                        <td><input type="number" name="data[Jan][gol_3_4]"></td>
-                        <td><input type="number" name="data[Jan][gol_1_2]"></td>
-                        <td><input type="number" name="data[Jan][kampanye]"></td>
-                        <td><input type="number" name="data[Jan][honor]"></td>
-                        <td><input type="number" name="data[Jan][pens_3_4]"></td>
-                        <td><input type="number" name="data[Jan][pens_1_2]"></td>
-                        <td><input type="number" name="data[Jan][direksi]"></td>
-                        <td><input type="number" name="data[Jan][dekom]"></td>
-                        <td><input type="number" name="data[Jan][pengacara]"></td>
-                        <td><input type="number" name="data[Jan][transport]"></td>
-                        <td><input type="number" name="data[Jan][hiperkes]"></td>
-                        <td><input type="number" name="data[Jan][total]"></td>
-                    </tr>
 
-                    <tr>
-                        <td><input type="text" value="Feb" name="data[Feb][bulan]" readonly></td>
-                        <td><input type="number" name="data[Feb][gol_3_4]"></td>
-                        <td><input type="number" name="data[Feb][gol_1_2]"></td>
-                        <td><input type="number" name="data[Feb][kampanye]"></td>
-                        <td><input type="number" name="data[Feb][honor]"></td>
-                        <td><input type="number" name="data[Feb][pens_3_4]"></td>
-                        <td><input type="number" name="data[Feb][pens_1_2]"></td>
-                        <td><input type="number" name="data[Feb][direksi]"></td>
-                        <td><input type="number" name="data[Feb][dekom]"></td>
-                        <td><input type="number" name="data[Feb][pengacara]"></td>
-                        <td><input type="number" name="data[Feb][transport]"></td>
-                        <td><input type="number" name="data[Feb][hiperkes]"></td>
-                        <td><input type="number" name="data[Feb][total]"></td>
-                    </tr>
+    <div class="scroll-buttons">
+      <button onclick="scrollTable('left')">⇦ Scroll Kiri</button>
+      <button onclick="scrollTable('right')">Scroll Kanan ⇨</button>
+    </div>
 
-                    <tr>
+    <div class="table-wrapper" id="tableWrapper">
+      <!-- Tempatkan tabelmu di sini -->
+      <form method="POST" action="/rekap-biaya/simpan">
+        <table>
+          <thead>
+            <tr>
+              <th rowspan="2">Rekap Bulan</th>
+              <th colspan="9" class="group-header">REAL BIAYA</th>
+              <th rowspan="2">Transport</th>
+              <th rowspan="2">Hiperkes</th>
+              <th rowspan="2">Total Biaya Kesehatan</th>
+            </tr>
+
+            <tr>
+              <th>Gol. III-IV</th>
+              <th>Gol. I-II</th>
+              <th>Kampanye</th>
+              <th>Honor + ILA + OS</th>
+              <th>Pens. III-IV</th>
+              <th>Pens. I-II</th>
+              <th>Direksi</th>
+              <th>Dekom</th>
+              <th>Pengacara</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><input type="text" value="Jan" name="data[Jan][bulan]" readonly></td>
+              <td><input type="number" name="data[Jan][gol_3_4]"></td>
+              <td><input type="number" name="data[Jan][gol_1_2]"></td>
+              <td><input type="number" name="data[Jan][kampanye]"></td>
+              <td><input type="number" name="data[Jan][honor]"></td>
+              <td><input type="number" name="data[Jan][pens_3_4]"></td>
+              <td><input type="number" name="data[Jan][pens_1_2]"></td>
+              <td><input type="number" name="data[Jan][direksi]"></td>
+              <td><input type="number" name="data[Jan][dekom]"></td>
+              <td><input type="number" name="data[Jan][pengacara]"></td>
+              <td><input type="number" name="data[Jan][transport]"></td>
+              <td><input type="number" name="data[Jan][hiperkes]"></td>
+              <td><input type="number" name="data[Jan][total]"></td>
+            </tr>
+
+            <tr>
+              <td><input type="text" value="Feb" name="data[Feb][bulan]" readonly></td>
+              <td><input type="number" name="data[Feb][gol_3_4]"></td>
+              <td><input type="number" name="data[Feb][gol_1_2]"></td>
+              <td><input type="number" name="data[Feb][kampanye]"></td>
+              <td><input type="number" name="data[Feb][honor]"></td>
+              <td><input type="number" name="data[Feb][pens_3_4]"></td>
+              <td><input type="number" name="data[Feb][pens_1_2]"></td>
+              <td><input type="number" name="data[Feb][direksi]"></td>
+              <td><input type="number" name="data[Feb][dekom]"></td>
+              <td><input type="number" name="data[Feb][pengacara]"></td>
+              <td><input type="number" name="data[Feb][transport]"></td>
+              <td><input type="number" name="data[Feb][hiperkes]"></td>
+              <td><input type="number" name="data[Feb][total]"></td>
+            </tr>
+
+            <tr>
                         <td><input type="text" value="Mar" name="data[Mar][bulan]" readonly></td>
                         <td><input type="number" name="data[Mar][gol_3_4]"></td>
                         <td><input type="number" name="data[Mar][gol_1_2]"></td>
@@ -138,9 +171,9 @@
                         <td><input type="number" name="data[Mar][transport]"></td>
                         <td><input type="number" name="data[Mar][hiperkes]"></td>
                         <td><input type="number" name="data[Mar][total]"></td>
-                    </tr>
+            </tr>
 
-                    <tr>
+            <tr>
                         <td><input type="text" value="Apr" name="data[Apr][bulan]" readonly></td>
                         <td><input type="number" name="data[Apr][gol_3_4]"></td>
                         <td><input type="number" name="data[Apr][gol_1_2]"></td>
@@ -154,9 +187,9 @@
                         <td><input type="number" name="data[Apr][transport]"></td>
                         <td><input type="number" name="data[Apr][hiperkes]"></td>
                         <td><input type="number" name="data[Apr][total]"></td>
-                    </tr>
+            </tr>
 
-                    <tr>
+            <tr>
                         <td><input type="text" value="May" name="data[May][bulan]" readonly></td>
                         <td><input type="number" name="data[May][gol_3_4]"></td>
                         <td><input type="number" name="data[May][gol_1_2]"></td>
@@ -170,8 +203,9 @@
                         <td><input type="number" name="data[May][transport]"></td>
                         <td><input type="number" name="data[May][hiperkes]"></td>
                         <td><input type="number" name="data[May][total]"></td>
-                    </tr>
-                    <tr>
+            </tr>
+            
+            <tr>
                         <td><input type="text" value="Jun" name="data[Jun][bulan]" readonly></td>
                         <td><input type="number" name="data[Jun][gol_3_4]"></td>
                         <td><input type="number" name="data[Jun][gol_1_2]"></td>
@@ -185,8 +219,9 @@
                         <td><input type="number" name="data[Jun][transport]"></td>
                         <td><input type="number" name="data[Jun][hiperkes]"></td>
                         <td><input type="number" name="data[Jun][total]"></td>
-                    </tr>
-                    <tr>
+            </tr>
+            
+            <tr>
                         <td><input type="text" value="Jul" name="data[Jul][bulan]" readonly></td>
                         <td><input type="number" name="data[Jul][gol_3_4]"></td>
                         <td><input type="number" name="data[Jul][gol_1_2]"></td>
@@ -200,8 +235,9 @@
                         <td><input type="number" name="data[Jul][transport]"></td>
                         <td><input type="number" name="data[Jul][hiperkes]"></td>
                         <td><input type="number" name="data[Jul][total]"></td>
-                    </tr>
-                    <tr>
+            </tr>
+            
+            <tr>
                         <td><input type="text" value="Aug" name="data[Aug][bulan]" readonly></td>
                         <td><input type="number" name="data[Aug][gol_3_4]"></td>
                         <td><input type="number" name="data[Aug][gol_1_2]"></td>
@@ -215,8 +251,9 @@
                         <td><input type="number" name="data[Aug][transport]"></td>
                         <td><input type="number" name="data[Aug][hiperkes]"></td>
                         <td><input type="number" name="data[Aug][total]"></td>
-                    </tr>
-                    <tr>
+            </tr>
+            
+            <tr>
                         <td><input type="text" value="Sep" name="data[Sep][bulan]" readonly></td>
                         <td><input type="number" name="data[Sep][gol_3_4]"></td>
                         <td><input type="number" name="data[Sep][gol_1_2]"></td>
@@ -230,8 +267,9 @@
                         <td><input type="number" name="data[Sep][transport]"></td>
                         <td><input type="number" name="data[Sep][hiperkes]"></td>
                         <td><input type="number" name="data[Sep][total]"></td>
-                    </tr>
-                    <tr>
+            </tr>
+            
+            <tr>
                         <td><input type="text" value="Oct" name="data[Oct][bulan]" readonly></td>
                         <td><input type="number" name="data[Oct][gol_3_4]"></td>
                         <td><input type="number" name="data[Oct][gol_1_2]"></td>
@@ -245,8 +283,9 @@
                         <td><input type="number" name="data[Oct][transport]"></td>
                         <td><input type="number" name="data[Oct][hiperkes]"></td>
                         <td><input type="number" name="data[Oct][total]"></td>
-                    </tr>
-                    <tr>
+            </tr>
+            
+            <tr>
                         <td><input type="text" value="Nov" name="data[Nov][bulan]" readonly></td>
                         <td><input type="number" name="data[Nov][gol_3_4]"></td>
                         <td><input type="number" name="data[Nov][gol_1_2]"></td>
@@ -260,8 +299,9 @@
                         <td><input type="number" name="data[Nov][transport]"></td>
                         <td><input type="number" name="data[Nov][hiperkes]"></td>
                         <td><input type="number" name="data[Nov][total]"></td>
-                    </tr>
-                    <tr>
+            </tr>
+            
+            <tr>
                         <td><input type="text" value="Dec" name="data[Dec][bulan]" readonly></td>
                         <td><input type="number" name="data[Dec][gol_3_4]"></td>
                         <td><input type="number" name="data[Dec][gol_1_2]"></td>
@@ -275,13 +315,25 @@
                         <td><input type="number" name="data[Dec][transport]"></td>
                         <td><input type="number" name="data[Dec][hiperkes]"></td>
                         <td><input type="number" name="data[Dec][total]"></td>
-                    </tr>
+            </tr>
+          </tbody>
+        </table>
 
-        </tbody>
-      </table>
-
-      <button type="submit" class="submit-btn">Simpan Rekap</button>
-    </form>
+        <button type="submit" class="submit-btn">Simpan Rekap</button>
+      </form>
+    </div>
   </div>
+
+  <script>
+    function scrollTable(direction) {
+      const wrapper = document.getElementById("tableWrapper");
+      const scrollAmount = 200; // pixel
+      if (direction === 'left') {
+        wrapper.scrollLeft -= scrollAmount;
+      } else {
+        wrapper.scrollLeft += scrollAmount;
+      }
+    }
+  </script>
 </body>
 </html>
