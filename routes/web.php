8f3b2cfb04bce0d7784a456c5b6705b9e8769773
airtaprtw <airtapertiwi870@gmail.com>
2025-07-06 19:41:16 +0700

    perbarui eror (4)

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,6 @@ Route::get('/content/data-obat', function () {
     return view('partials.data-obat'); 
 });
 
-use App\Http\Controllers\RekapBiayaController;
-
 Route::get('/content/rekap-biaya', [RekapBiayaController::class, 'index'])->name('rekap-biaya.index');
 Route::post('/content/rekap-biaya', [RekapBiayaController::class, 'store'])->name('rekap-biaya.store');
 Route::get('/content/rekap-biaya/export', [RekapBiayaController::class, 'export'])->name('rekap-biaya.export');
